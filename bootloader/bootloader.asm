c193945e0656f792e8a0725c87b68da87ea4aa44
ZernTeam <104980440+entiflags@users.noreply.github.com>
2025-05-22 17:08:06 +0600

    [BBOOT] working with disk (part 1)

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -12,15 +12,57 @@ if ~ msg eq si
 end if
 }
 
-mov si, hello
-call panicfunc
+; stack end: 0x500
+; sp, bp: 0x600
+
+.start:
+    cli
+    cld
+    xor ax, ax
+    mov ss, ax
+    mov es, ax
+    mov sp, 600h
+    mov bp, 600h
+    push cs
+    pop ds
+
+.check_disk:
+	mov byte [boot_drive], dl
+	cmp dl, byte 080h
+	jl .not_hdd
+.is_hdd:
+;AH		41h = function number for extensions check[8]
+;DL  	drive index (e.g. 1st HDD = 80h)
+;BX		55AAh	
+	mov ah, byte 41h
+	mov bx, word 55AAh
+	int 13h
+;CF 	Set On Not Present, Clear If Present
+;BX		55AAh
+;CX		bit1: Device Access using the packet structure
+	jc .not_lba
+	cmp bx, 0AA55h
+	jne .not_lba
+	test cl, byte 1	; and cmp
+	jnz .lba_ok
+	
+.not_lba:	
+	mov si, lba_not_found
+	jmp panicfunc
+.not_hdd:
+	mov si, not_hdd
+	jmp panicfunc
+
+.lba_ok:
+
+
 
 forever:
     jmp forever
     
 ;usage
 ; mov si, STR_ADDR
-; call panicfunc
+; jmp panicfunc
 panicfunc:
 	print panic_prefix
 	print si
@@ -43,8 +85,14 @@ printfunc:
 .printfunc_end:
     ret
 
-panic_prefix:	db "BOOT PANIC: ",0
-hello:          db "Hello, boot world!",0
+; variables
+boot_drive			db 0
 
-pad:            db 510 - ($ - $$) dup 0
-boot_sign:      db 55h,0AAh
+; error messages
+panic_prefix:		db "BOOT PANIC: ",0
+lba_not_found:      db "Hard drive doesnt support LBA packet struct",0
+not_hdd:            db "Not a valid harddrive",0
+
+; padding and boot signature
+pad:            	db 510 - ($ - $$) dup 0
+boot_sign:      	db 55h,0AAh

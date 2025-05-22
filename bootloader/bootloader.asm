8e659d757de89540290abe98228429bbb6c3bf91
Kross1de <awesome.hubygu@yandex.ru>
2025-05-22 15:22:16 +0600

    [BBOOT] panic function

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -12,17 +12,28 @@ if ~ msg eq si
 end if
 }
 
-start:
-	print hello
+mov si, hello
+call panicfunc
 
 forever:
     jmp forever
+    
+;usage
+; mov si, STR_ADDR
+; call panicfunc
+panicfunc:
+	print panic_prefix
+	print si
+	xor ax, ax
+	int 16h
+	; the first instr 0xFFFF00
+	jmp far 0FFFFh:0
 
 ;usage
 ; mov si, STR_ADDR
 ; call printfunc
 printfunc:
-    lodsb                   ; loads [si] into al then (since DF = 0) si++#000000#000000
+    lodsb                   ; loads [si] into al then (since DF = 0) si++
     or al, al               ; if al == 0
     jz .printfunc_end
     mov ah, byte 0Eh        ; setup the INT10,E interrupt
@@ -32,6 +43,7 @@ printfunc:
 .printfunc_end:
     ret
 
+panic_prefix:	db "BOOT PANIC: ",0
 hello:          db "Hello, boot world!",0
 
 pad:            db 510 - ($ - $$) dup 0

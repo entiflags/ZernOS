3a506db7804b5aebbd748a3d3c17642aceed3d7a
Kross1de <awesome.hubygu@yandex.ru>
2025-05-22 14:42:19 +0600

    [BBOOT] preparing to create print to screen func

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,17 +1,7 @@
 org 0x7C00
 use16
 
-mov ah, 0Eh
-mov bx, 03h
-mov al, 'h'
-int 10h
-mov al, 'e'
-int 10h
-mov al, 'l'
-int 10h
-int 10h
-mov al, 'o'
-int 10h
+hello:          db "Hello, boot world!",0
 
-pad:            times 510 - ($ - $$) db 0
+pad:            db 510 - ($ - $$) dup 0
 boot_sign:      db 55h,0AAh

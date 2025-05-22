54941e8d9ac9a5c29707eec82b858865042116ee
ZernTeam <104980440+entiflags@users.noreply.github.com>
2025-05-22 14:36:54 +0600

    [BBOT] printing 'hello' to the screen

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -5,6 +5,13 @@ mov ah, 0Eh
 mov bx, 03h
 mov al, 'h'
 int 10h
+mov al, 'e'
+int 10h
+mov al, 'l'
+int 10h
+int 10h
+mov al, 'o'
+int 10h
 
 pad:            times 510 - ($ - $$) db 0
 boot_sign:      db 55h,0AAh

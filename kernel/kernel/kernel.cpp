9b2d236673c574abf5255c8693937f213dc4bf0f
entiflags <fishka_2023@mail.ru>
2025-05-30 15:28:28 +0600

    Add %p to printf, Changed into higher half kernel

--- kernel/kernel/kernel.cpp
+++ kernel/kernel/kernel.cpp
@@ -1,10 +1,17 @@
 #include <kernel/tty.h>
 
 #include <stdio.h>
+#include <stdint.h>
 
 extern "C"
 void kernel_main()
 {
 	terminal_initialize();
-	printf("Hello from the kernel!\n");
+
+	printf("Hello from the PineappleOS!\n");
+
+	printf("%p\n", kernel_main);
+
+	int a = 10;
+	printf("%p\n", &a);
 }

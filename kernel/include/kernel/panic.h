9e51378b5084c950b7db35c32c00a0eba3e4203e
entiflags <fishka_2023@mail.ru>
2025-06-05 11:32:03 +0600

    Kernel panic uses kprint with parameters

--- kernel/include/kernel/panic.h
+++ kernel/include/kernel/panic.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <kernel/kprint.h>
+
 namespace Kernel
 {
+	template<typename... Args>
         __attribute__((__noreturn__))
-        void panic(const char* message);
+	static void panic(const char* message, Args... args)
+	{
+		kprint("Kernel panic: ");
+		kprint(message, args...);
+		kprint("\n");
+		asm volatile("hlt");
+		__builtin_unreachable();
+	}
 }
